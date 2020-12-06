e8363ac9a49e676ad8d5157f7b61a629bc9a9870
Etudiant O'clock <etudiant@teleporteur.oclock>
2020-12-06 17:45:45 +0100

    BREAD de Genre avec un formulaire DeleteType aussi

--- src/Controller/Admin/GenreController.php
+++ src/Controller/Admin/GenreController.php
@@ -2,34 +2,120 @@
 
 namespace App\Controller\Admin;
 
-
 use App\Entity\Genre;
+use App\Form\DeleteType;
 use App\Form\GenreType;
+use App\Repository\GenreRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
+/**
+     * @Route("/admin/genre", name="admin_genre_")
+     */
 class GenreController extends AbstractController
 {
     /**
-     * @Route("/admin/genre/new", name="admin_genre_new")
+     * @Route("/", name="browse")
      */
-    public function new(Request $request): Response
+    public function browse(GenreRepository $genreRepository): Response
     {
-        $genre=new Genre();
-        //$genre->setName('Thriller');
-        $form=$this->createForm(GenreType::class, $genre);
         
+        return $this->render('admin/genre/browse.html.twig',[
+            'genres'=>$genreRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/edit/{id}", name="edit", requirements={"id":"\d+"})
+     */
+
+    public function edit(Genre $genre, Request $request): Response
+    {
+        $form=$this->createForm(GenreType::class,$genre);
+
         $form->handleRequest($request);
 
         if($form->isSubmitted() && $form->isValid()){
-            dd('formulaire envoyé et valide');
+
+            $genre->setUpdatedAt(new \DateTime());
+
+            $em=$this->getDoctrine()->getManager();
+
+            $em->flush();
         }
 
+        // $formDelete=$this->createFormBuilder()
+        //                 ->setAction($this->generateUrl('admin_genre_delete',['id'=>$genre->getId()]))
+        //                 ->setMethod('DELETE')
+        //                 ->add('deleteButton',SubmitType::class,[
+        //                     'label'=>'Supprimer',
+        //                 ]) 
+        //                 ->getForm();
 
-        return $this->render('admin/genre/new.html.twig', [
-            'form'=>$form->createView()
+        $formDelete=$this->createForm(DeleteType::class,null, [
+            'action'=>$this->generateUrl('admin_genre_delete',['id'=>$genre->getId()])
         ]);
+
+        return $this->render('admin/genre/edit.html.twig',[
+
+            'form'=>$form->createView(),
+
+            'formDelete'=>$formDelete->createView(),
+        ]);
+       
+    }
+
+    /**
+     * @Route("/add", name="add")
+     */
+
+    public function add(Request $request): Response
+    {
+        $genre=new Genre();
+
+        $form=$this->createForm(GenreType::class, $genre);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+
+            $em=$this->getDoctrine()->getManager();
+            $em->persist($genre);
+            $em->flush();
+
+            return $this->redirectToRoute('admin_genre_browse');
+        }
+
+        return $this->render('admin/genre/add.html.twig',[
+            'form'=>$form->createView(),
+        ]);
+       
+    }
+
+   /**
+     * @Route("/delete/{id}", name="delete", requirements={"id":"\d+"})
+     */
+
+    public function delete(EntityManagerInterface $em, Genre $genre, Request $request): Response
+       {
+
+            $formDelete=$this->createForm(DeleteType::class);
+
+            $formDelete->handleRequest($request);
+
+            if($formDelete->isSubmitted() && $formDelete->isValid()){
+    
+                // $em=$this->getDoctrine()->getManager();
+                $em->persist($genre);
+                $em->flush();
+    
+            }
+            
+            return $this->redirectToRoute('admin_genre_browse');
     }
 }
--- src/Form/DeleteType.php
+++ src/Form/DeleteType.php
@@ -2,31 +2,27 @@
 
 namespace App\Form;
 
-use App\Entity\Genre;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\Length;
-use Symfony\Component\Validator\Constraints\NotBlank;
 
-class GenreType extends AbstractType
+class DeleteType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            //->add('name',TextareaType::class)jj
-           //->add('submit', SubmitType::class)
-          ->add('name')
+        ->setMethod('DELETE')
+        ->add('deleteButton',SubmitType::class,[
+            'label'=>'Supprimer',
+        ]) 
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => Genre::class,
+            // Configure your form options here
         ]);
     }
 }
